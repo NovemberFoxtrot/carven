cf63b7f747a97ac84b450b23f7e535a2e9300f4f
NovemberFoxtrot <november@fox.trot>
2013-08-07 02:23:06 -0700

    still

--- carven.go
+++ carven.go
@@ -21,33 +21,35 @@ func main() {
 		t := strings.Split(somestring, " ")
 
 		for i := 0; i < (len(t) - 1); i++ {
-      k1 := t[i]     + "##" + t[i+1]
-      k2 := t[i + 1] + "##" + t[i]
+			k1 := t[i] + "##" + t[i+1]
+			k2 := t[i+1] + "##" + t[i]
 
 			s[k1] += 1
 			s[k2] += 1
 		}
 	}
 
-  v := make(map[string]map[string]int)
+	v := make(map[string]map[string]int)
 
-  for key, _ := range s{
-    keys := strings.Split(key, "##")
-    
-    v[keys[0]] = make(map[string]int)
-  }
+	for key, value := range s {
+		keys := strings.Split(key, "##")
 
-  for key, value := range s{
-    keys := strings.Split(key, "##")
-    
-    v[keys[0]][keys[1]] = value
-  }
+		v[keys[0]] = make(map[string]int)
 
-  for key, value := range v{
-    if len(value) > 1 {
-      fmt.Println(key, value)
-    }
-  }
+		v[keys[0]][keys[0]] = value
+	}
 
-  fmt.Println("Goodnight. Ding-ding-ding-ding-ding- ding-ding-ding.")
+	for key, value := range s {
+		keys := strings.Split(key, "##")
+
+		v[keys[0]][keys[1]] = value
+	}
+
+	for key, value := range v {
+		if len(value) > 1000 {
+			fmt.Println(key, value)
+		}
+	}
+
+	fmt.Println("Goodnight. Ding-ding-ding-ding-ding- ding-ding-ding.")
 }
