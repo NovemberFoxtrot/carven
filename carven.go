20fbf3954c7169a50a22de84239f65f64df70003
NovemberFoxtrot <november@fox.trot>
2013-08-08 21:31:59 -0700

    getting places

--- carven.go
+++ carven.go
@@ -2,60 +2,162 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"log"
+	"math"
 	"os"
+	"regexp"
 	"runtime"
 	"strings"
-  "fmt"
-  "math"
 )
 
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+func CalcDotProduct(v1, v2 []float64) float64 {
+	result := 0.0
 
+	for index, _ := range v1 {
+		result += v1[index] * v2[index]
+	}
+
+	return result
+}
+
+func CalcMagnitude(v []float64) float64 {
+	if len(v) == 0 {
+		return 0.0
+	}
+
+	var subtotal float64
+
+	for _, somefloat := range v {
+		subtotal += float64(somefloat * somefloat)
+	}
+
+	return math.Sqrt(subtotal)
+}
+
+func BuildVector(outerkey, innerkey string, outervalue, innervalue map[string]int) ([]float64, []float64) {
+	maxlength := len(outervalue) * len(innervalue)
+
+	outervector := make([]float64, 0, maxlength)
+	innervector := make([]float64, 0, maxlength)
+
+	commonkeys := make(map[string]bool)
+
+	for outerstring, _ := range outervalue {
+		commonkeys[outerstring] = true
+	}
+
+	for innerstring, _ := range innervalue {
+		commonkeys[innerstring] = true
+	}
+
+	if len(commonkeys) == 0 {
+		return outervector, innervector
+	}
+
+	for commonstring, commonbool := range commonkeys {
+		if commonbool == true {
+			outervector = append(outervector, float64(outervalue[commonstring]))
+			innervector = append(innervector, float64(innervalue[commonstring]))
+		}
+	}
+
+	return outervector, innervector
+}
+
+func CalcCosim(v1, v2 []float64) float64 {
+	dotproduct := CalcDotProduct(v1, v2)
+
+	if dotproduct > 0 {
+		v1m := CalcMagnitude(v1)
+		v2m := CalcMagnitude(v2)
+
+		return dotproduct / (v1m * v2m)
+	} else {
+		return 0.0
+	}
+}
+
+func CleanString(s string) string {
+	reg, err := regexp.Compile("[^A-Za-z0-9 ]+")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	safe := reg.ReplaceAllString(s, "")
+	safe = strings.ToLower(strings.Trim(safe, ""))
+	safe = strings.TrimSpace(safe)
+
+	return safe
+}
+
+func Parse(s ParseData) {
 	scanner := bufio.NewScanner(os.Stdin)
-
-	s := make(map[string]int)
-	v := make(map[string]map[string]int)
 
 	for scanner.Scan() {
 		somestring := scanner.Text()
 
-		t := strings.Split(somestring, " ")
+		somestring = CleanString(somestring)
 
-		for i := 0; i < (len(t) - 1); i++ {
-			k1 := t[i] + "##" + t[i+1]
-			k2 := t[i+1] + "##" + t[i]
+		if len(somestring) > 0 {
+			t := strings.Split(somestring, " ")
 
-			s[k1] += 1
-			s[k2] += 1
+			if len(t) > 1 {
+				for i := 0; i < (len(t) - 1); i++ {
+					k1 := t[i] + "##" + t[i+1]
+					k2 := t[i+1] + "##" + t[i]
+
+					s[k1] += 1
+					s[k2] += 1
+				}
+			}
 		}
 	}
+}
 
+func BuildMultiSet(s ParseData, v MultiSetData) {
 	for key, value := range s {
 		keys := strings.Split(key, "##")
 
-    if v[keys[0]] == nil {
-		  v[keys[0]] = make(map[string]int)
+		if v[keys[0]] == nil {
+			v[keys[0]] = make(map[string]int)
+		} else {
+			keys := strings.Split(key, "##")
 
-      // think about this one
-		  // v[keys[0]][keys[0]] = value
-    } else {
-		  keys := strings.Split(key, "##")
+			v[keys[0]][keys[1]] = value
+		}
 
-		  v[keys[0]][keys[1]] = value
-    }
 	}
+}
 
-	for key, value := range v {
-    dotproduct := 0
-    magnitude := 0
+type ParseData map[string]int
+type MultiSetData map[string]map[string]int
 
-    for subkey, subvalue := range value {
-      // fmt.Println(key, subkey)
-      magnitude += subvalue * subvalue
-      dotproduct *= subvalue
-    }
-    fmt.Println(dotproduct, math.Sqrt(float64(magnitude)))
-  }
-  fmt.Println(v)
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	p := make(ParseData)
+	m := make(MultiSetData)
+
+	Parse(p)
+	BuildMultiSet(p, m)
+
+	var v1 []float64
+	var v2 []float64
+	var cosign float64
+	var innerkey string
+	var outerkey string
+	var innervalue map[string]int
+	var outervalue map[string]int
+
+	for outerkey, outervalue = range m {
+		for innerkey, innervalue = range m {
+			v1, v2 = BuildVector(outerkey, innerkey, outervalue, innervalue)
+
+			if cosign = CalcCosim(v1, v2); cosign > 0.8 {
+				fmt.Println(outerkey, innerkey, "\t\t", math.Floor(cosign*100))
+			}
+		}
+	}
 }
